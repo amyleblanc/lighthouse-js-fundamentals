901056606eadf9e5e13211b14886a48551e1f357
Amy <amymccarthy2014@gmail.com>
2021-10-05 02:26:34 +0000

    for and while loops over arrays

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,12 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
-console.log(packingList[3]);
+
+//for (let i = 0; i < packingList.length; i++) {
+//  console.log(packingList[i]);
+//}
